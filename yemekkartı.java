96e40e0057787d665ceab8c45c7c4802384f34a5
edaunay <117381758+edaunay@users.noreply.github.com>
2022-11-03 20:31:53 +0300

    Create yemekkartı.java

---
+++ yemekkartı.java
@@ -0,0 +1,67 @@
+package yemekKarti;
+
+public class yemekkartı {
+	private String username;
+	private int password;
+	private int bakiye;
+	
+	public yemekKarti(string username , int password , int bakiye);
+	       this.username = username;
+	       this.password = password;
+	       this.bakiye = bakiye;
+	       
+	       
+}
+
+  void removeBakiye(int foodId) {
+	  
+	  if(bakiye <=0) {
+		  
+		      system.out.println("Yetersiz bakiye!");
+		      
+		      {else {
+		    	  if(foodId =1) {
+		    		   bakiye = bakiye  - 29;
+		    		   {else if(foodId = 2) {
+		    			   bakiye = bakiye - 20;
+		    			   {else if(foodId = 3) {
+		    				   bakiye = bakiye - 18;
+		    						   {else if(foodId =4) {
+		    							   bakiye = bakiye -38;
+		    									   {else if(foodId =5) {
+		    										   bakiye = bakiye -46;
+		    												   
+		    									   }
+		    									   System.out.println("Islem basarili,guncel bakiyeniz:" + bakiye);
+		    						   }
+		    									   
+		    			   }
+		    						
+		    void showBakiye(){
+		    	System.out.println("Guncel bakiyeniz:" + bakiye);
+		    	
+		    }
+		    
+		    
+		    void bakiyeYukle(int newBakiye) {
+		    	bakiye += newBakiye;
+		    	System.out.println("Paraniz basariyla yatirilmnistir");
+		    	System.out.println("Guncel bakiyeniz: *+ bakiye");
+		    }
+		    
+		    string getUsername() {
+		    	return username;
+		    }
+		    }
+		    	
+		    }
+		    	}
+		    }
+		    						   
+
+		    	  }
+		      }
+	  }
+	  
+	  
+  }
